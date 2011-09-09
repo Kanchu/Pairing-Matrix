fd2b9fa14647bc9653d4ab6b953feaaae40fcad7
Kanchan Motlag <kanchan.motlag09@gmail.com>
2011-09-10 00:21:41 +0530

    matrix

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matrix::Application.config.secret_token = '0a98eeee6df3045ab30ca28453c20e410331d42f8670cd942efc6faf3eebe0a17b80f26f670f600a52fb2e3f2351241226dca8a35d6fc15bdbac1ac950686fd1'
